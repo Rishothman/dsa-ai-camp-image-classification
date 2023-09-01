ec36306535cd5ee26b89bb41397e643949ae7682
Rishan Rishothman <117252053+Rishothman@users.noreply.github.com>
2023-09-01 13:21:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ function gotResult(error, results) {
   label = results[0].label;
 
   // play sound
-  if (label == 'Class3') {
+  if (label == 'Class 3') {
     counter++;
     // Play sound if "helmet" is continuously predicted for 5 times
     if (counter >= 2) {
